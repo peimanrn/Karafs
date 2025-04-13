2534cf555e57f658c0ba3b683d8abc991f320941
Peiman <peiman.ramezaninejad@gmail.com>
2025-04-13 15:24:05 +0330

    Update data model.

--- Karafs/Models/Product.swift
+++ Karafs/Models/Product.swift
@@ -22,7 +22,7 @@ struct ProductResponse: Codable {
     }
 }
 
-struct Product: Codable {
+struct Product: Codable, Identifiable {
     let id: Int
     let title: String
     let description: String
@@ -70,5 +70,55 @@ struct Product: Codable {
         case thumbnail
         case images
     }
+
+    static var dummyData: Product {
+        Product(
+            id: 1,
+            title: "iPhone 16 Pro Max",
+            description: "The best iPhone in the world",
+            category: "Phone",
+            price: 2000,
+            discountPercentage: 5,
+            rating: 4.5,
+            stock: 24,
+            tags: ["Phone", "Apple"],
+            brand: "Apple",
+            sku: "iphone16promax256gb",
+            weight: 400,
+            dimensions: Dimensions(width: 200, height: 150, depth: 50),
+            warrantyInformation: "1 Year",
+            shippingInformation: "By Air",
+            availabilityStatus: "Available",
+            reviews: [
+                Review(
+                    rating: 5,
+                    comment: "A great phone",
+                    date: Date(),
+                    reviewerName: "Ali",
+                    reviewerEmail: "alimail@mail.com"
+                ),
+                Review(
+                    rating: 4.3,
+                    comment: "A great phone but heavy",
+                    date: Date(),
+                    reviewerName: "Mohammad",
+                    reviewerEmail: "momohami@gmail.com"
+                ),
+            ],
+            returnPolicy: "If dammaged return within 7 days",
+            minimumOrderQuantity: 1,
+            meta: Meta(
+                createdAt: Date(),
+                updatedAt: Date(),
+                barcode: "123123123123",
+                qrCode: URL(string: "https://assets.dummyjson.com/public/qr-code.png")!
+            ),
+            thumbnail: URL(string: "https://cdn.dummyjson.com/products/images/beauty/Red%20Nail%20Polish/thumbnail.png")!,
+            images: [
+                URL(string: "https://cdn.dummyjson.com/products/images/beauty/Red%20Nail%20Polish/1.png")!
+            ]
+        )
+    }
+
 }
 
