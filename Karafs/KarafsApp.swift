4fc1ed6b2eed6f9d9cf4c7cf9983117d03743cc6
Peiman <peiman.ramezaninejad@gmail.com>
2025-04-13 09:48:08 +0330

    Initial Commit

---
+++ Karafs/KarafsApp.swift
@@ -0,0 +1,17 @@
+//
+//  KarafsApp.swift
+//  Karafs
+//
+//  Created by Peiman Ramezaninejad on 4/13/25.
+//
+
+import SwiftUI
+
+@main
+struct KarafsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
